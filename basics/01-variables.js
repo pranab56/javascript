0b569b52ae0f2efcb3ca7a81fdd670ca6ed7b7d7
Pranab kumar <96796886+pranab56@users.noreply.github.com>
2023-11-26 18:22:01 +0000

    varibale for js

---
+++ basics/01-variables.js
@@ -0,0 +1,12 @@
+const userId = '21345';
+let userEmail = "pronab@gmail.com";
+var userPassowrd = "345678";
+userGender = "male";
+
+
+/*
+1.prefer not to use var;
+2.because of essu block scope and functional scope;
+*/
+
+console.table([userId,userEmail,userPassowrd,userGender])
